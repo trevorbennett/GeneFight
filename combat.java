2af61db35abecdc69815960fd74cc08ff81ea7c4
trevorbennett <tb684611@ohio.edu>
2016-11-19 14:29:38 -0500

    Create combat.java

---
+++ combat.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+/**
+ * Created by D532387 on 11/19/2016.
+ */
+public class Combat {
+
+    private Boolean didUserWinCombat;
+    private Boolean isCombatOngoing;
+    private Fighter userFighter;
+    private Fighter enemyFighter;
+
+    private Double userRemainingHealth;
+    private Double enemyRemainingHealth;
+
+    Combat(Fighter userFighter, Fighter enemyFighter){
+        this.userFighter = userFighter;
+        this.enemyFighter = enemyFighter;
+    }
+
+    public Boolean fight(){
+        isCombatOngoing = true;
+        while(isCombatOngoing){
+            nextTurn();
+            checkForCombatEnd();
+        }
+        return didUserWinCombat;
+    }
+
+    private void checkForCombatEnd() {
+        if(enemyFighter.getHealth() <= 0){
+            isCombatOngoing = false;
+            didUserWinCombat = true;
+        }
+        if(userFighter.getHealth() <= 0){
+            isCombatOngoing = false;
+            didUserWinCombat = false;
+        }
+    }
+
+    private Double attack(Double attackerAttack, Double DefenderHealth) {
+        return DefenderHealth - (attackerAttack * Math.random());
+    }
+
+    private void nextTurn(){
+        enemyRemainingHealth = attack(userFighter.getAttack(),enemyFighter.getHealth());
+        enemyFighter.setHealth(enemyRemainingHealth);
+        userRemainingHealth = attack(enemyFighter.getAttack(),userFighter.getHealth());
+        userFighter.setHealth(userRemainingHealth);
+    }
+}
